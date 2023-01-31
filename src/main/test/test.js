3e29bac3170f1d08448918a4524cc75cbdccccff
ahmedsmiai <47064156+ahmedsmiai@users.noreply.github.com>
2023-01-31 14:12:17 +0100

    added metric for issuing duration

--- src/main/test/test.js
+++ src/main/test/test.js
@@ -1,11 +1,12 @@
 import http from 'k6/http'
-import { Counter } from 'k6/metrics'
+import { Counter, Trend } from 'k6/metrics'
 import { sleep, check as loadTestingCheck } from "k6";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
-
 let failedTestCases = new Counter('failedTestCases');
+const issuing_duration = new Trend('issuing_duration');
+
 
 export const options = {
     duration: '1m',
@@ -26,22 +27,19 @@ let check = function (obj, conditionArray, tags) {
 
 export default function () {
     const url = "https://core.ssikit.walt.id/v1/did/create";
-
     let didData = {
         "method": "key"
     }
     const url1 = 'https://signatory.ssikit.walt.id/v1/credentials/issue';
-
     const url2 = 'https://auditor.ssikit.walt.id/v1/verify';
-
+   
     let did1 = (http.post(url, JSON.stringify(didData), {
         headers: { 'Content-Type': 'application/json' },
     }))
-
     let did2 = (http.post(url, JSON.stringify(didData), {
         headers: { 'Content-Type': 'application/json' },
     }))
-
+   
     let data = {
         "templateId": "OpenBadgeCredential",
         "config": {
@@ -52,7 +50,9 @@ export default function () {
 
     let res = http.post(url1, JSON.stringify(data), {
         headers: { 'Content-Type': 'application/json' },
-    })
+    });
+    issuing_duration.add(res.timings.waiting);
+    console.log(issuing_duration.name); // waiting_time
 
     let auditorData = {
         "policies":
@@ -67,35 +67,28 @@ export default function () {
             ]
     }
 
+
     let auditRes = http.post(url2, JSON.stringify(auditorData), {
         headers: { 'Content-Type': 'application/json' },
     })
-
-
     check(did1.status, {
         "Creating did API is working": (status) => status === 200
     });
-
     check(res.status, {
         "Issuing OBC credential API is working": (status) => status === 200
     });
-
-
     check(res.json('issuer.id'), {
         "credential creator OK": (creator) => creator === did1.body
     })
-
     check(res.json('credentialSubject.id'), {
         "credential holder OK": (id) => id === did2.body
     })
-
     check(auditRes.json('valid'), {
         "valid credential": (valid) => valid === true
     })
 }
 export function handleSummary(data) {
     console.log('Finished executing performance tests');
-
     return {
         'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
         "result.html": htmlReport(data),
