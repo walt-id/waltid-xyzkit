5343c2e958ea57e0ca4b2b8e619d84dd763ab4d1
Philipp Potisk <phil@walt.id>
2023-09-05 18:35:10 +0200

    chore: updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ import com.github.jk1.license.render.ReportRenderer
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.20"
-    id("io.ktor.plugin") version "2.2.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+
+    id("io.ktor.plugin") version "2.3.4"
     id("org.owasp.dependencycheck") version "6.5.3"
     id("com.github.jk1.dependency-license-report") version "2.0"
     application
@@ -30,52 +31,53 @@ dependencies {
     /* -- KTOR -- */
 
     // Ktor server
-    implementation("io.ktor:ktor-server-core-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-auth-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-sessions-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-auto-head-response-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-double-receive-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-host-common-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-status-pages-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-compression-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-cors-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-forwarded-header-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-call-logging-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-call-id-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-cio-jvm:2.2.4")
+    implementation("io.ktor:ktor-server-core-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-auth-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-sessions-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-auto-head-response-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-double-receive-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-host-common-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-status-pages-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-compression-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-cors-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-forwarded-header-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-call-logging-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-call-id-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-cio-jvm:2.3.4")
 
     // Ktor server external libs
+    // TODO upgrade 2.4.0 or use: implementation("io.ktor:ktor-server-swagger:2.3.4")
     implementation("io.github.smiley4:ktor-swagger-ui:1.4.0")
 
     // Ktor client
-    implementation("io.ktor:ktor-client-core:2.2.4")
-    implementation("io.ktor:ktor-client-serialization:2.2.4")
-    implementation("io.ktor:ktor-client-content-negotiation:2.2.4")
-    implementation("io.ktor:ktor-client-json:2.2.4")
-    implementation("io.ktor:ktor-client-cio:2.2.4")
-    implementation("io.ktor:ktor-client-logging-jvm:2.2.4")
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-serialization:2.3.4")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-client-json:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation("io.ktor:ktor-client-logging-jvm:2.3.4")
 
 
     /* -- Kotlin -- */
 
     // Kotlinx.serialization
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.2.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.4")
 
     // Date
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
 
     /* -- Database --*/
 
     // DB
-    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.43.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.43.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.43.0")
     implementation("org.xerial:sqlite-jdbc:3.41.2.1")
     //implementation("org.postgresql:postgresql:42.5.4")
 
@@ -87,9 +89,9 @@ dependencies {
 
     // Logging
     implementation("io.github.oshai:kotlin-logging-jvm:4.0.0-beta-22")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("org.slf4j:jul-to-slf4j:2.0.7")
-    implementation("io.ktor:ktor-client-cio-jvm:2.2.4")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("org.slf4j:jul-to-slf4j:2.0.9")
+    implementation("io.ktor:ktor-client-cio-jvm:2.3.4")
 
     // Test
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8.20")
@@ -97,11 +99,11 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
     testImplementation("io.kotest:kotest-assertions-core:5.5.5")
     testImplementation("io.kotest.extensions:kotest-assertions-ktor:2.0.0")
-    testImplementation("io.ktor:ktor-server-tests-jvm:2.2.4")
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.4")
 
     // CLI
-    implementation("com.github.ajalt.clikt:clikt-jvm:3.5.0")
-    implementation("com.github.ajalt.clikt:clikt:3.5.0")
+    implementation("com.github.ajalt.clikt:clikt-jvm:3.5.2")
+    implementation("com.github.ajalt.clikt:clikt:3.5.2")
 
 }
 
